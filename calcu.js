9608fe967eb937457e1f217e0f77290bdaf258f1
teusdrz <136101749+teusdrz@users.noreply.github.com>
2023-10-05 17:29:23 -0300

    Create calcu.js

---
+++ calcu.js
@@ -0,0 +1,95 @@
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+const input = document.getElementById("input");
+const resultInput = document.getElementById("result");
+
+const allowedkeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  "%",
+  "",
+];
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", function () {
+    const value = charKeyBtn.dataset.value;
+    input.value += value;
+  });
+});
+
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = "";
+  input.focus();
+});
+
+// keydown = quando uma tecla  é pressionada
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
+  if (allowedkeys.includes(ev.key)) {
+    // um evento onde desabilita qualquer letra na calculadora somente numero
+    input.value += ev.key;
+    return;
+  }
+  if (ev.key === "Backspace") {
+    // permite backspace/apagar na calculadora
+    input.value = input.value.slice(0, -1);
+  }
+  if (ev.key === "Enter") {
+    calculate();
+  }
+});
+
+document.getElementById("equal").addEventListener("click", calculate);
+
+function calculate() {
+  resultInput.value = "ERROR";
+  resultInput.classList.add("error");
+  const result = eval(input.value);
+  resultInput.value = result;
+  resultInput.classList.remove("error");
+}
+
+document
+  .getElementById("copyToClipboard")
+  .addEventListener("click", function (ev) {
+    const button = ev.currentTarget;
+    if (button.innerText === "Copy") {
+      button.innerText = "Copied";
+      button.classList.add("success");
+      navigator.clipboard.writeText(resultInput.value);
+    } else {
+      button.innerText = "Copy";
+      button.classList.remove("success");
+    }
+  });
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("primary-color", "#26834a");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("primary-color", "#4dff91");
+    main.dataset.theme = "dark";
+  }
+});
